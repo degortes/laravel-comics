adbbf1878af998b1a14871d8254762bb09f30d65
degortes <danilodegortes@gmal.com>
2021-01-09 12:02:06 +0100

    start details section

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -23,19 +23,104 @@
         <div class="container">
             <div class="expanded-info">
                 <h1 class="text-up">{{$comic['title']}}</h1>
+                <div class="info-bar">
+                    <div class="info-left">
+                        <p>U.S. Price <span>{{$comic['price']}}</span> </p>
+                        <p class="text-up">available</p>
+                    </div>
+                    <div class="info-right">
+                        <p>Check Availability ▼ </p>
+                    </div>
+                </div>
+                <div class="description">
+                    <p>{{$comic['description']}}</p>
+                </div>
 
             </div>
             <div class="advertising">
                 <h4 class="text-up">advertisement</h4>
                 <img src="{{asset('images/adv.jpg')}}" alt="">
+            </div>
+        </div>
+    </section>
+    <section id="summary">
+        <div class="container">
+            <div class="talent">
+                <h2>Talent</h2>
+                <ul>
+                    <li>
+                        <span>Art By</span>
+                        <div class="right-sum">
+                            @foreach ($comic['artists'] as $artist)
+                                <a href="#">{{$artist}}@if (!$loop->last),@endif</a>
+                                @endforeach
+                        </div>
+                    </li>
+                    <li>
+                        <span>Written By</span>
+                        <div class="right-sum">
+                            @foreach ($comic['writers'] as $artist)
+                                <a href="#">{{$artist}}@if (!$loop->last),@endif</a>
+                                @endforeach
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="specs">
+                <h2>Specs</h2>
+                <ul>
+                    <li>
+                        <span>Series</span>
+                        <div class="right-sum">
+                            <a href="#">{{$comic['series']}}</a>
+                        </div>
+                    </li>
+                    <li>
+                        <span>U.S. Price</span>
+                        <div class="right-sum">
+                            <a href="#">{{$comic['price']}}</a>
+                        </div>
+                    </li>
+                    <li>
+                        <span>On Sale Date:</span>
+                        <div class="right-sum">
+                            <a href="#">{{$comic['sale_date']}}</a>
+                        </div>
+                    </li>
+                    <li>
+                        <span>Volume/Issue #:</span>
+                        <div class="right-sum">
+                            <a href="#">1000</a>
+                        </div>
+                    </li>
+                    <li>
+                        <span>Trim Size:</span>
+                        <div class="right-sum">
+                            <a href="#">1000</a>
+                        </div>
+                    </li>
+                    <li>
+                        <span>Page Count:</span>
+                        <div class="right-sum">
+                            <a href="#">1000</a>
+                        </div>
+                    </li>
+                    <li>
+                        <span>Date:</span>
+                        <div class="right-sum">
+                            <a href="#">1000</a>
+                        </div>
+                    </li>
+
+                </ul>
+
 
             </div>
 
 
         </div>
-
-
     </section>
+
 
 </main>
 @endsection
